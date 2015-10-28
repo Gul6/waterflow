5ffcb379c38a2747277a3fe668546181a77f4e10
Ebadullah Gul <ebadgul66@gmail.com>
2015-10-28 19:25:25 +0000

    have new sticky footer, added new page ie. technology, fixed up colors

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'homes#index'
+  
   get 'about_us' => 'homes#about_us'
-  
-  
+  get 'technology' => 'homes#technology'
   
   
   # Example of regular route:
