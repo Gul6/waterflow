d0b6c6f109df81075c0b6e3a953fd22cd9b26c47
Ebadullah Gul <ebadgul66@gmail.com>
2015-10-28 21:43:42 +0000

    added full name column for user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151020203246) do
+ActiveRecord::Schema.define(version: 20151028211410) do
 
   create_table "models", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20151020203246) do
     t.string   "name"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "full_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
