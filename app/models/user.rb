e6c7387d8cdf5e4fb0c5fe1109d52cc7e4a99dc7
Ebadullah Gul <ebadgul66@gmail.com>
2015-11-14 21:32:06 +0000

    removed rating and post id, show full name of user who posted

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ActiveRecord::Base
   validates :full_name, presence: true
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "50x50>" }, :default_url => "/images/:style/default_image.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+ 
+  has_many :posts
 end
